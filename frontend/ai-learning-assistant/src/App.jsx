19d6e67d56856cf1288dabbc56b3c1754300750d
futuristic-coder <raman17100100@gmail.com>
2026-01-26 21:31:54 +0530

    Project Structure

---
+++ frontend/ai-learning-assistant/src/App.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./pages/Auth/LoginPage";
+import RegisterPage from "./pages/Auth/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
+import DocumentListPage from "./pages/Documents/DocumentListPage";
+import DocumentDetailPage from "./pages/Documents/DocumentDetailPage";
+import FlashcardPage from "./pages/Flashcards/FlashcardPage";
+import FlashcardsListPage from "./pages/Flashcards/FlashcardsListPage";
+import ProfilePage from "./pages/Profile/ProfilePage";
+import QuizResultPage from "./pages/Quizzes/QuizResultPage";
+import QuizTakePage from "./pages/Quizzes/QuizTakePage";
+import ProctedRoute from "./components/auth/ProctedRoute";
+
+const App = () => {
+  const isAuthenticated = true;
+  const loading = false;
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        Loading...
+      </div>
+    );
+  }
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Navigate to="/dashboard" replace />
+            ) : (
+              <Navigate to="/login" replace />
+            )
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Protected Routes */}
+        <Route element={<ProctedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/document" element={<DocumentListPage />} />
+          <Route path="/documents/:id" element={<DocumentDetailPage />} />
+          <Route path="/flashcards" element={<FlashcardsListPage />} />
+          <Route path="/documents/:id/flashcards" element={<FlashcardPage />} />
+          <Route path="/quizzes/:quidId" element={<QuizTakePage />} />
+          <Route path="/quizzes/:quidId/results" element={<QuizResultPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
