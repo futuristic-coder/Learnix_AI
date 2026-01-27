c9db99965889d595dd9f85aa5a19631f69dfca05
futuristic-coder <raman17100100@gmail.com>
2026-01-27 23:07:41 +0530

    Database Schemas

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,10 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import connectDB from "./config/db.js"
-import errorHandler from './middleware/errorHandler.js'
+import connectDB from "./config/db.js";
+import errorHandler from './middleware/errorHandler.js';
 
+import authRoutes from "./routes/authRoutes.js"
 
 const __filename= fileURLToPath(import.meta.url);
 const __dirname= path.dirname(__filename);
@@ -32,6 +33,7 @@ app.use(express.urlencoded({extended:true }));
 
 app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
+app.use('/api/auth',authRoutes)
 
 app.use(errorHandler);
 
