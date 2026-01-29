a90c43bf82ee2722f28dddeaffa0c4f228bdc7e4
futuristic-coder <raman17100100@gmail.com>
2026-01-29 22:46:05 +0530

    Document APIs

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ import connectDB from "./config/db.js";
 import errorHandler from './middleware/errorHandler.js';
 
 import authRoutes from "./routes/authRoutes.js"
+import documentRoutes from "./routes/documentRoutes.js"
+
 
 const __filename= fileURLToPath(import.meta.url);
 const __dirname= path.dirname(__filename);
@@ -34,6 +36,7 @@ app.use(express.urlencoded({extended:true }));
 app.use('/uploads', express.static(path.join(__dirname,'uploads')));
 
 app.use('/api/auth',authRoutes)
+app.use('/api/documents',documentRoutes)
 
 app.use(errorHandler);
 
